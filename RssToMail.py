af882704c65a7fc6183bd55122324f91ec0e27a0
Farnault Jeremy <jeremy.farnault@gfi.fr>
2016-10-19 11:54:24 +0200

    Modif check rss

--- RssToMail.py
+++ RssToMail.py
@@ -3,25 +3,65 @@
 import feedparser
 import datetime
 
-d = feedparser.parse('http://www.gamekult.com/feeds/actu.html')
-feedparser.registerDateHandler("W3CDTF")
+print("RSS TO MAIL " + str(datetime.now()) + " ...")
 
-#print(d['feed']['title'])
-
-#print(d['feed']['link'])
-
-#print(d.feed.subtitle)
-
-#print(len(d['entries']))
-
-#print(d['entries'][0]['title'])
-
-#print(d.entries[0]['link'])
-
+# Variables
+today_articles = list()
 year = datetime.datetime.now().year
 month = datetime.datetime.now().month
 day = datetime.datetime.now().day
 
-for post in d.entries:
-    if post.published_parsed[0] == year and post.published_parsed[1] == month and post.published_parsed[2] == 18:
-        print(post.published + " --> " + post.title + ": " + post.link + "")
+# Websites to check
+feed_list = ['http://www.gamekult.com/feeds/actu.html',
+             'http://http://www.journaldugeek.com/feed/',
+             'https://www.geeksaresexy.net/feed/',
+             'http://korben.info/feed',
+             'http://www.elbakin.net/rss.php',
+             'http://www.penofchaos.com/naheulbeukrss.xml']
+
+# Gathering feeds
+for feed in feed_list:
+    d = feedparser.parse(feed)
+    for post in d.entries:
+        if post.published_parsed[0] == year and \
+           post.published_parsed[1] == month and \
+           post.published_parsed[2] == day:
+            today_articles.append("<li><a href={}>{} - {}</a></li>".format(post.link, d['feed']['title'], post.title))
+
+# Send a mail
+msg = MIMEMultipart()
+msg['From'] = "haskkor@gmail.com"
+msg['To'] = "haskkor@gmail.com"
+msg['Subject'] = "[RSS FEEDS] News - {}".format(now)
+html = """\
+<html>
+    <head>
+        NEWS LIST
+    </head>
+    <body>
+        <ul>
+            {}
+        </ul>
+    </body>
+</html>
+""".format(''.join(today_articles))
+msg.attach(MIMEText(html, 'html'))
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("haskkor@gmail.com", "gkpkqtmaxlonnrry")
+text = msg.as_string()
+server.sendmail("haskkor@gmail.com", "haskkor@gmail.com", text)
+server.quit()
+
+print("OK\n")
+
+#print(d['feed']['title'])
+#print(d['feed']['link'])
+#print(d.feed.subtitle)
+#print(len(d['entries']))
+#print(d['entries'][0]['title'])
+#print(d.entries[0]['link'])
+
+
+
+
